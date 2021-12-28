5d3bd1a5c5b83b5726d02028c9a8ce81b1656d7d
Stefan <stefan@burdy.io>
2021-12-28 23:24:20 +0100

    feat: implement streamlined rewrites; add tests

--- packages/web-utils/rewrites/create-rewrites.ts
+++ packages/web-utils/rewrites/create-rewrites.ts
@@ -1,10 +1,8 @@
 import { RewriteOptions, RewritesObject } from '../types';
-import { matchAndRewrite, matchAndRewriteMap } from './build-path';
+import { matchAndRewriteMap } from './build-path';
 
-export const createRewrites = <T extends {}>(options: RewriteOptions<T>): RewritesObject<T> => ({
+export const createRewrites = <T extends {} | string>(options: RewriteOptions<T>): RewritesObject<T> => ({
   getOrigin: () => options?.origin || '',
-  getRewriteMap: () => options.rewriteMap,
-  getRewrite: () => options.rewrite,
-  rewrite: (path: string) => matchAndRewrite(path, options.rewrite),
-  rewriteMap: (path: string) => matchAndRewriteMap(path, options.rewriteMap, options?.origin || '')
+  getRewrite: () => options?.rewrite || [],
+  rewrite: (path: string) => matchAndRewriteMap(path, options.rewrite, options?.origin || '')
 });
